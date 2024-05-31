a50bb7c15e27ba789325acb9c9523f04f848c76f
safna <fathimathu29@gmail.com>
2024-05-31 21:36:15 +0530

    add new page

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
-const mongoClient=require('mongodb').MongoClient
+const mongoClient = require("mongodb").MongoClient;
 
+let db = null;
 
-let db = null
+module.exports.connect = function () {
+  const url = "mongodb://localhost:27017";
+  const dbname = "shopping";
 
-module.exports.connect=function(){
-    const url='mongodb://localhost:27017'
-    const dbname='shopping'
-
-    mongoClient.connect(url)
-  .then((data) => {
-    console.log('successfully connected to database...') 
-    db=data.db(dbname)
-  })
-  .catch((err) => {
-    console.log('error occured'+err)
-  })
-
-}
-module.exports.get=function(){
-    return db
-}
+  mongoClient
+    .connect(url)
+    .then((data) => {
+      console.log("successfully connected to database...");
+      db = data.db(dbname);
+    })
+    .catch((err) => {
+      console.log("error occured" + err);
+    });
+};
+module.exports.get = function () {
+  return db;
+};
--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,63 +1,74 @@
-var db=require('../config/connection')
-var collection=require('../config/collections')
-var {ObjectId}=require('mongodb')
-module.exports={
+var db = require("../config/connection");
+var collection = require("../config/collections");
+var { ObjectId } = require("mongodb");
+module.exports = {
+  addProduct: (product, callback) => {
+    console.log(product);
 
-    addProduct:(product,callback)=>{
-console.log(product)
-
-        db.get().collection('product').insertOne(product).then((data)=>{
-            callback(data.insertedId)       
-
+    db.get()
+      .collection("product")
+      .insertOne(product)
+      .then((data) => {
+        callback(data.insertedId);
+      });
+  },
+  getAllProducts: () => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db
+        .get()
+        .collection(collection.PRODUCT_COLLECTION)
+        .find()
+        .toArray();
+      resolve(products);
+    });
+  },
+  deleteProduct: (prodId) => {
+    return new Promise((resolve, reject) => {
+      console.log(prodId);
+      console.log(new ObjectId(prodId)); // Change ObjectId to new ObjectId()
+      db.get()
+        .collection(collection.PRODUCT_COLLECTION)
+        .deleteOne({ _id: new ObjectId(prodId) }) // Change removeOne to deleteOne
+        .then((response) => {
+          resolve(response);
         })
-    },
-    getAllProducts:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
-            resolve(products)
-        })
-    },
-    deleteProduct: (prodId) => {
-        return new Promise((resolve, reject) => {
-            console.log(prodId);
-            console.log(new ObjectId(prodId)); // Change ObjectId to new ObjectId()
-            db.get() .collection(collection.PRODUCT_COLLECTION) .deleteOne({ _id: new ObjectId(prodId) }) // Change removeOne to deleteOne
-                .then((response) => {
-                    resolve(response);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
+        .catch((err) => {
+          reject(err);
         });
-    },
-    getProductDetails:(prodId)=>{
-       return new Promise((resolve,reject)=>{
-        db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:new ObjectId(prodId)}).then((product)=>{
-            resolve(product)
-        })
-       }) 
-    },
-    updateProduct:(prodId,proDetails)=>{
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id: new ObjectId(prodId)},{
-                $set:{
-                    name:proDetails.name,
-                    description:proDetails.description,
-                    price:proDetails.price,
-                    category:proDetails.category,
-                    type:proDetails.type,
-                    color:proDetails.color,
-                    material:proDetails.material,
-                    brand:proDetails.brand
-                }
-            }).then((response)=>{
-                resolve()
-            })
-        })
-    }
-
-}
-
-
-
-
+    });
+  },
+  getProductDetails: (prodId) => {
+    return new Promise((resolve, reject) => {
+      db.get()
+        .collection(collection.PRODUCT_COLLECTION)
+        .findOne({ _id: new ObjectId(prodId) })
+        .then((product) => {
+          resolve(product);
+        });
+    });
+  },
+  updateProduct: (prodId, proDetails) => {
+    return new Promise((resolve, reject) => {
+      db.get()
+        .collection(collection.PRODUCT_COLLECTION)
+        .updateOne(
+          { _id: new ObjectId(prodId) },
+          {
+            $set: {
+              name: proDetails.name,
+              description: proDetails.description,
+              price: proDetails.price,
+              category: proDetails.category,
+              type: proDetails.type,
+              color: proDetails.color,
+              material: proDetails.material,
+              brand: proDetails.brand,
+            },
+          }
+        )
+        .then((response) => {
+          resolve();
+        });
+    });
+  },
+};
